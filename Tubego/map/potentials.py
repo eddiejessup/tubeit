fec066977ec151a2a53947707abe7bd296084386
Elliot Marsden <elliot.marsden@gmail.com>
2013-05-02 20:11:34 +0100

    Remove unused code from potentials

--- Tubego/map/potentials.py
+++ Tubego/map/potentials.py
@@ -1,6 +1,5 @@
 from __future__ import print_function
 import numpy as np
-import utils
 
 def LJ(r_0, U_0):
     '''
@@ -36,24 +35,10 @@ def harm_osc(k):
         return 0.5 * k * r_sq ** 2
     return func
 
-def harm_osc_F(k):
-    '''
-    Harmonic oscillator, F(r) = -k * r.
-    '''
-    def func(r):
-        return -k * r
-    return func
-
 def logistic(r_0, U_0, k):
     ''' Logistic approximation to step function. '''
     def func(r_sq):
         return 0.5 * U_0 * (1.0 + np.tanh(k * (np.sqrt(r_sq) - r_0)))
-    return func
-
-def logistic_F(r_0, U_0, k):
-    def func(r):
-        r_sq = utils.vector_mag(r)
-        return -U_0 * utils.vector_unit_nonull(r) * (1.0 - np.square(np.tanh(k * (np.sqrt(r_sq) - r_0))))[:, np.newaxis]
     return func
 
 def polar_rose(n):
@@ -65,13 +50,3 @@ def polar_rose_sq(n):
     def func(t):
         return 1.5-np.cos(0.5 * n * t) ** 2
     return func
-
-def polar_null():
-    def func(t):
-        return np.ones_like(t)
-    return func
-
-def anis_wrap(func_iso, func_polar):
-    def func_anis(r_sq, theta):
-        return func_iso(r_sq) * func_polar(theta)
-    return func_anis
